9de8d677cc6ce14d588304f5ce0e18f49eb89437
Chris Laplante <chris.laplante@agilent.com>
2024-04-30 15:49:07 -0400

    WIP expand

--- examples/petgraph2.rs
+++ examples/petgraph2.rs
@@ -1,14 +1,19 @@
-use std::cell::{Ref, RefCell};
+use std::borrow::Cow;
+use std::cell::{RefCell};
 use std::cmp::Ordering;
-use std::collections::BTreeSet;
+use std::collections::{BTreeSet, HashSet};
 
+use anyhow::Context;
 use fxhash::FxHashMap;
 use once_cell::sync::Lazy;
 use petgraph::dot::Dot;
 use petgraph::graph::NodeIndex;
 use petgraph::prelude::StableGraph;
 use petgraph::stable_graph::DefaultIx;
-use regex::Regex;
+use regex::{Captures, Regex};
+use scopeguard::{defer, guard, ScopeGuard};
+use bytebraise::data_smart::errors::{DataSmartError, DataSmartResult};
+use bytebraise::data_smart::utils::ReplaceFallible;
 
 static VAR_EXPANSION_REGEX: Lazy<Regex> =
     Lazy::new(|| Regex::new(r"\$\{[a-zA-Z0-9\-_+./~]+?}").unwrap());
@@ -108,9 +113,23 @@ impl<T: Ord> FifoHeap<T> {
 }
 
 #[derive(Debug)]
+struct ExpansionState {
+    visited: HashSet<String>,
+}
+
+impl ExpansionState {
+    pub fn new() -> Self {
+        ExpansionState {
+            visited: HashSet::new(),
+        }
+    }
+}
+
+#[derive(Debug)]
 struct DataSmart {
     ds: StableGraph<GraphItem, ()>,
     vars: FxHashMap<String, NodeIndex<DefaultIx>>,
+    expand_state: RefCell<Option<ExpansionState>>,
 }
 
 impl DataSmart {
@@ -118,6 +137,7 @@ impl DataSmart {
         DataSmart {
             ds: StableGraph::new(),
             vars: FxHashMap::default(),
+            expand_state: RefCell::new(None),
         }
     }
 
@@ -160,6 +180,67 @@ impl DataSmart {
         self.ds.add_edge(*var_entry, stmt_idx, ());
     }
 
+    fn expand<S: AsRef<str>>(&self, value: S) -> DataSmartResult<String> {
+        let value = value.as_ref();
+
+        // |expand_state| is used to track which variables are accessed during an expansion, across
+        // recursive calls to this method. The first (i.e. non-recursive) call to this method is
+        // responsible for setting up and tearing down the state.
+
+        // Create a scope guard that will clear out the expansion state upon scope exit
+        let scope = guard((), |()| {
+            RefCell::borrow_mut(&self.expand_state).take();
+        });
+
+        // Check if we are in the middle of recursion by seeing if the expand state exists yet
+        {
+            let mut s = RefCell::borrow_mut(&self.expand_state);
+            if s.is_none() {
+                // The expansion state doesn't exist yet, so create it
+                *s = Some(ExpansionState::new());
+            } else {
+                // Expansion state exists; defuse scope guard - we are not responsible for the state
+                ScopeGuard::into_inner(scope);
+            }
+        }
+
+        let mut value = value.to_string();
+        while value.contains("${") {
+            let new_value = VAR_EXPANSION_REGEX.replace_fallible(value.as_ref(), |caps: &Captures| -> DataSmartResult<String> {
+                let match_str = caps.get(0).unwrap().as_str();
+                let referenced_var = &match_str[2..match_str.len() - 1];
+
+                {
+                    let mut s = RefCell::borrow_mut(&self.expand_state);
+                    let set = s.as_mut().unwrap();
+                    if set.visited.contains(referenced_var) {
+                        return Err(DataSmartError::RecursiveReferenceError {
+                            var: referenced_var.to_string(),
+                        }
+                            .into());
+                    } else {
+                        set.visited.insert(referenced_var.to_string());
+                    }
+                }
+
+                defer! {
+                        let mut s = RefCell::borrow_mut(&self.expand_state);
+                        let set = s.as_mut().unwrap();
+                        set.visited.remove(referenced_var);
+                    }
+
+                Ok(self.get_var(referenced_var).unwrap_or(match_str.to_string()))
+            })?;
+
+            if value == new_value {
+                break;
+            }
+            value = new_value.to_string();
+        }
+
+        Ok(value)
+    }
+
     pub fn get_var<S: AsRef<str>>(&self, var: S) -> Option<String> {
         let var = var.as_ref();
 
@@ -186,14 +267,15 @@ impl DataSmart {
                     ret = ret.map(|s| {
                         s + " " + &*q.statement().rhs.clone()
                     });
-                },
+                }
                 StmtKind::Assign => {
                     ret = q.statement().rhs.clone().into();
-                },
+                }
                 _ => unimplemented!()
             }
         }
 
+        ret = ret.map(|s| self.expand(s).unwrap());
         *cached = ret.clone();
 
         ret
@@ -305,10 +387,10 @@ fn main() {
     //parse_value("${${M}}");
 
     println!("\n");
-    let ret = d.get_var("A");
+    let ret = d.get_var("OVERRIDES");
     println!("\n");
 
-    println!("ret = {:?}", ret);
+    println!("ret = {:?}\n", ret);
 
     println!("{:?}", Dot::with_config(&d.ds, &[]));
 }
