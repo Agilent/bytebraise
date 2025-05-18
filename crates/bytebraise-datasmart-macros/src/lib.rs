0f182f209541b484ddd90e67d05f1f035b8e7c32
Chris Laplante <chris.laplante@agilent.com>
2025-05-18 19:17:39 -0400

    bytebraise-datasmart-macros: wip

---
+++ crates/bytebraise-datasmart-macros/src/lib.rs
@@ -0,0 +1,35 @@
+use proc_macro::TokenStream;
+use quote::quote;
+use syn::{
+    Expr, Lit, Token,
+    parse::{Parse, ParseStream},
+    parse_macro_input,
+};
+
+/// Parsed input: `<target>, <value>`
+struct MacroInput {
+    target: Expr,
+    _comma: Token![,],
+    value: Expr,
+}
+
+impl Parse for MacroInput {
+    fn parse(input: ParseStream<'_>) -> syn::Result<Self> {
+        let target = input.parse()?;
+        let _comma = input.parse()?;
+        let value = input.parse()?;
+        Ok(Self {
+            target,
+            _comma,
+            value,
+        })
+    }
+}
+
+#[proc_macro]
+pub fn m(item: TokenStream) -> TokenStream {
+    // Expect exactly: `<target> , <expr>`
+    let MacroInput { target, value, .. } = parse_macro_input!(item as MacroInput);
+
+    quote!( #target.set(#value) ).into()
+}
